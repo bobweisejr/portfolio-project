bf579d86e85b905787107a9d1e70e4f3ec193012
Robert Weise <Robert.Weise@spartannash.com>
2019-07-24 13:42:20 -0400

    Added Hex Reverse functionality

---
+++ wordcount/hex_function.py
@@ -0,0 +1,3 @@
+def hex_function1(hex_value):
+    hex_dict = {'0000':'0', '0001':'1', '0010':'2', '1111':'F'}
+    return hex_dict[hex_value]
--- wordcount/views.py
+++ wordcount/views.py
@@ -170,4 +170,124 @@ def hex_reverse(request):
 
 
 def hex_reverse_calculated(request):
-    return render(request, 'hex_reverse_calculated.html')
+    # hex_text_reverse = request.GET['hex_reverse']
+    Byte1Bit1 = request.GET['Byte1Bit1']
+    Byte1Bit2 = request.GET['Byte1Bit2']
+    Byte1Bit3 = request.GET['Byte1Bit3']
+    Byte1Bit4 = request.GET['Byte1Bit4']
+    Byte1Bit5 = request.GET['Byte1Bit5']
+    Byte1Bit6 = request.GET['Byte1Bit6']
+    Byte1Bit7 = request.GET['Byte1Bit7']
+    Byte1Bit8 = request.GET['Byte1Bit8']
+    Byte1Half1 = str(Byte1Bit1 + Byte1Bit2 + Byte1Bit3 + Byte1Bit4).strip()
+    Byte1Half2 = str(Byte1Bit5 + Byte1Bit6 + Byte1Bit7 + Byte1Bit8).strip()
+    Byte1Hex = hex_function(Byte1Half2[::-1]) + hex_function(Byte1Half1[::-1])
+
+    # Byte2
+    Byte2Bit1 = request.GET['Byte2Bit1']
+    Byte2Bit2 = request.GET['Byte2Bit2']
+    Byte2Bit3 = request.GET['Byte2Bit3']
+    Byte2Bit4 = request.GET['Byte2Bit4']
+    Byte2Bit5 = request.GET['Byte2Bit5']
+    Byte2Bit6 = request.GET['Byte2Bit6']
+    Byte2Bit7 = request.GET['Byte2Bit7']
+    Byte2Bit8 = request.GET['Byte2Bit8']
+    Byte2Half1 = str(Byte2Bit1 + Byte2Bit2 + Byte2Bit3 + Byte2Bit4).strip()
+    Byte2Half2 = str(Byte2Bit5 + Byte2Bit6 + Byte2Bit7 + Byte2Bit8).strip()
+    Byte2Hex = hex_function(Byte2Half2[::-1]) + hex_function(Byte2Half1[::-1])
+
+    Byte3Bit1 = request.GET['Byte3Bit1']
+    Byte3Bit2 = request.GET['Byte3Bit2']
+    Byte3Bit3 = request.GET['Byte3Bit3']
+    Byte3Bit4 = request.GET['Byte3Bit4']
+    Byte3Bit5 = request.GET['Byte3Bit5']
+    Byte3Bit6 = request.GET['Byte3Bit6']
+    Byte3Bit7 = request.GET['Byte3Bit7']
+    Byte3Bit8 = request.GET['Byte3Bit8']
+    Byte3Half1 = str(Byte3Bit1 + Byte3Bit2 + Byte3Bit3 + Byte3Bit4).strip()
+    Byte3Half2 = str(Byte3Bit5 + Byte3Bit6 + Byte3Bit7 + Byte3Bit8).strip()
+    Byte3Hex = hex_function(Byte3Half2[::-1]) + hex_function(Byte3Half1[::-1])
+
+    Byte4Bit1 = request.GET['Byte4Bit1']
+    Byte4Bit2 = request.GET['Byte4Bit2']
+    Byte4Bit3 = request.GET['Byte4Bit3']
+    Byte4Bit4 = request.GET['Byte4Bit4']
+    Byte4Bit5 = request.GET['Byte4Bit5']
+    Byte4Bit6 = request.GET['Byte4Bit6']
+    Byte4Bit7 = request.GET['Byte4Bit7']
+    Byte4Bit8 = request.GET['Byte4Bit8']
+    Byte4Half1 = str(Byte4Bit1 + Byte4Bit2 + Byte4Bit3 + Byte4Bit4).strip()
+    Byte4Half2 = str(Byte4Bit5 + Byte4Bit6 + Byte4Bit7 + Byte4Bit8).strip()
+    Byte4Hex = hex_function(Byte4Half2[::-1]) + hex_function(Byte4Half1[::-1])
+
+    Byte5Bit1 = request.GET['Byte5Bit1']
+    Byte5Bit2 = request.GET['Byte5Bit2']
+    Byte5Bit3 = request.GET['Byte5Bit3']
+    Byte5Bit4 = request.GET['Byte5Bit4']
+    Byte5Bit5 = request.GET['Byte5Bit5']
+    Byte5Bit6 = request.GET['Byte5Bit6']
+    Byte5Bit7 = request.GET['Byte5Bit7']
+    Byte5Bit8 = request.GET['Byte5Bit8']
+    Byte5Half1 = str(Byte5Bit1 + Byte5Bit2 + Byte5Bit3 + Byte5Bit4).strip()
+    Byte5Half2 = str(Byte5Bit5 + Byte5Bit6 + Byte5Bit7 + Byte5Bit8).strip()
+    Byte5Hex = hex_function(Byte5Half2[::-1]) + hex_function(Byte5Half1[::-1])
+
+    Byte6Bit1 = request.GET['Byte6Bit1']
+    Byte6Bit2 = request.GET['Byte6Bit2']
+    Byte6Bit3 = request.GET['Byte6Bit3']
+    Byte6Bit4 = request.GET['Byte6Bit4']
+    Byte6Bit5 = request.GET['Byte6Bit5']
+    Byte6Bit6 = request.GET['Byte6Bit6']
+    Byte6Bit7 = request.GET['Byte6Bit7']
+    Byte6Bit8 = request.GET['Byte6Bit8']
+    Byte6Half1 = str(Byte6Bit1 + Byte6Bit2 + Byte6Bit3 + Byte6Bit4).strip()
+    Byte6Half2 = str(Byte6Bit5 + Byte6Bit6 + Byte6Bit7 + Byte6Bit8).strip()
+    Byte6Hex = hex_function(Byte6Half2[::-1]) + hex_function(Byte6Half1[::-1])
+
+    Byte7Bit1 = request.GET['Byte7Bit1']
+    Byte7Bit2 = request.GET['Byte7Bit2']
+    Byte7Bit3 = request.GET['Byte7Bit3']
+    Byte7Bit4 = request.GET['Byte7Bit4']
+    Byte7Bit5 = request.GET['Byte7Bit5']
+    Byte7Bit6 = request.GET['Byte7Bit6']
+    Byte7Bit7 = request.GET['Byte7Bit7']
+    Byte7Bit8 = request.GET['Byte7Bit8']
+    Byte7Half1 = str(Byte7Bit1 + Byte7Bit2 + Byte7Bit3 + Byte7Bit4).strip()
+    Byte7Half2 = str(Byte7Bit5 + Byte7Bit6 + Byte7Bit7 + Byte7Bit8).strip()
+    Byte7Hex = hex_function(Byte7Half2[::-1]) + hex_function(Byte7Half1[::-1])
+
+    Byte8Bit1 = request.GET['Byte8Bit1']
+    Byte8Bit2 = request.GET['Byte8Bit2']
+    Byte8Bit3 = request.GET['Byte8Bit3']
+    Byte8Bit4 = request.GET['Byte8Bit4']
+    Byte8Bit5 = request.GET['Byte8Bit5']
+    Byte8Bit6 = request.GET['Byte8Bit6']
+    Byte8Bit7 = request.GET['Byte8Bit7']
+    Byte8Bit8 = request.GET['Byte8Bit8']
+    Byte8Half1 = str(Byte8Bit1 + Byte8Bit2 + Byte8Bit3 + Byte8Bit4).strip()
+    Byte8Half2 = str(Byte8Bit5 + Byte8Bit6 + Byte8Bit7 + Byte8Bit8).strip()
+    Byte8Hex = hex_function(Byte8Half2[::-1]) + hex_function(Byte8Half1[::-1])
+
+    BytesList = [Byte1Hex, Byte2Hex, Byte3Hex, Byte4Hex, Byte5Hex, Byte6Hex, Byte7Hex, Byte8Hex]
+    BytesCombined = " ".join(BytesList)
+
+    return render(request, 'hex_reverse_calculated.html', {'Byte1Half1': Byte1Half1, 'Byte2Half2': Byte2Half2,
+                                                           'Byte3Half2': Byte3Half2, 'Byte4Half2': Byte4Half2,
+                                                           'Byte5Half2': Byte5Half2, 'Byte6Half2': Byte6Half2,
+                                                           'Byte7Half2': Byte7Half2, 'Byte8Half2': Byte8Half2,
+                                                           'Byte1Hex': Byte1Hex, 'Byte2Hex': Byte2Hex,
+                                                           'Byte3Hex': Byte3Hex, 'Byte4Hex': Byte4Hex,
+                                                           'Byte5Hex': Byte5Hex, 'Byte6Hex': Byte6Hex,
+                                                           'Byte7Hex': Byte7Hex, 'Byte8Hex': Byte8Hex,
+                                                           'BytesCombined': BytesCombined})
+
+
+def hex_function(hex_value):
+    try:
+        hex_dict = {'0000': '0', '0001': '1', '0010': '2', '0011': '3', '0100': '4', '0101': '5', '0110': '6',
+                    '0111': '7',
+                    '1000': '8', '1001': '9', '1010': 'A', '1011': 'B', '1100': 'C', '1101': 'D', '1110': 'E',
+                    '1111': 'F'}
+        return hex_dict[hex_value]
+    except:
+        return ''
